dscorbett/duployan-font

commit 2fe8d6a4922b139a4bb6fcc4d870f5888f3de7d3

Simplify the implementation of `refresh`
Setting a glyph's `glyphname` triggers a call to `GlyphHashFree`, which is
apparently all it takes to refresh the glyph name list.

font/duployan.py
@@ -18,7 +18,6 @@ __all__ = ['Builder']
 
 import collections
 import math
-import os
 import re
 import tempfile
 
@@ -1188,8 +1187,8 @@ class Builder(object):
         mark_glyphs = []
         for mark in schema.marks:
             mark_glyphs.append(self.draw_glyph(mark).glyphname)
-        self.refresh()
         glyph = self.font.createChar(schema.cp, glyph_name)
+        self._refresh(glyph)
         self.font.temporary[glyph_name] = glyph
         glyph.glyphclass = 'baseglyph'
         glyph.addReference(base_glyph)
@@ -1227,15 +1226,9 @@ class Builder(object):
         glyph.transform(psMat.translate(0, BASELINE - center))
         return glyph
 
-    def refresh(self):
+    def _refresh(self, glyph):
         # Work around https://github.com/fontforge/fontforge/issues/3278
-        sfd_path = 'refresh.sfd'
-        assert not os.path.exists(sfd_path), '{} already exists'.format(sfd_path)
-        self.font.save(sfd_path)
-        temporary = self.font.temporary
-        self.font = fontforge.open(sfd_path)
-        self.font.temporary = temporary
-        os.remove(sfd_path)
+        glyph.glyphname = glyph.glyphname
 
     def augment(self):
         add_lookups(self.font)
@@ -1244,7 +1237,6 @@ class Builder(object):
         merge_schemas(schemas, lookups, classes)
         for schema in schemas:
             glyph = self.draw_glyph(schema)
-        self.refresh()
         self.fea = '{}\n{}'.format(classes_str(classes), '\n'.join(map(str, lookups)))
 
     def merge_features(self, tt_font, old_fea):

commit 47b219e78fce62af30dc3e4ea605188d5168ced6

Let multiple code points map to one glyph
This was broken by 2fe8d6a4922b139a4bb6fcc4d870f5888f3de7d3.

font/duployan.py
@@ -1207,7 +1207,7 @@ class Builder(object):
         for mark in schema.marks:
             mark_glyphs.append(self.draw_glyph(mark).glyphname)
         glyph = self.font.createChar(schema.cp, glyph_name)
-        self._refresh(glyph)
+        self._refresh_glyph(glyph)
         self.font.temporary[glyph_name] = glyph
         glyph.glyphclass = 'baseglyph'
         glyph.addReference(base_glyph)
@@ -1245,9 +1245,13 @@ class Builder(object):
         glyph.transform(psMat.translate(0, BASELINE - center))
         return glyph
 
-    def _refresh(self, glyph):
+    def _refresh_glyph(self, glyph):
         # Work around https://github.com/fontforge/fontforge/issues/3278
         glyph.glyphname = glyph.glyphname
+
+    def _refresh_encoding(self):
+        # Work around https://github.com/fontforge/fontforge/issues/3551
+        self.font.encoding = self.font.encoding
 
     def augment(self):
         add_lookups(self.font)
@@ -1256,6 +1260,7 @@ class Builder(object):
         merge_schemas(schemas, lookups, classes)
         for schema in schemas:
             glyph = self.draw_glyph(schema)
+        self._refresh_encoding()
         class_asts = {}
         for name, schemas in sorted(classes.items()):
             class_ast = fontTools.feaLib.ast.GlyphClassDefinition(

tests/altuni.test
@@ -12,4 +12,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-00A0 1BC48 2060::[space=0+260|u1BC48.ie=1+220|uni2060.wj=2@170,-100+170]
+202F 1BC48 2060::[uni2009.thinSp=0+200|u1BC48.ie=1@-220,-100+0|uni2060.wj=2@170,-200+170]