d6775612206f4945bf83c42600249688e7033cb6
David Corbett <corbett.dav@husky.neu.edu>
2019-12-03 21:59:01 -0500

    Use Apache 2.0 license for the build script
    See <https://github.com/alif-type/amiri/commit/400b702ec65b8dbae19b64ef7c39f23e4eb40f9f>
    for the upstream license change.

--- font/build.py
+++ font/build.py
@@ -1,16 +1,17 @@
-# build.py - Duployan font build utility
+# Copyright 2010-2019 Khaled Hosny <khaledhosny@eglug.org>
+# Copyright 2018-2019 David Corbett
 #
-# Written in 2010-2018 by Khaled Hosny <khaledhosny@eglug.org>
-# Written in 2018-2019 by David Corbett <corbett.dav@husky.neu.edu>
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-# To the extent possible under law, the authors have dedicated all
-# copyright and related and neighboring rights to this software to the
-# public domain worldwide. This software is distributed without any
-# warranty.
+#     http://www.apache.org/licenses/LICENSE-2.0
 #
-# You should have received a copy of the CC0 Public Domain Dedication
-# along with this software. If not, see
-# <http://creativecommons.org/publicdomain/zero/1.0/>.
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 import argparse
 import os
