1e4fb4717509c88b0b35b836d32a517d0ce0da6c
David Corbett <corbett.dav@husky.neu.edu>
2020-04-18 22:22:36 -0400

    Update to FontTools 4.8.1, but allow back to 4.2.5
    Because font/build.py patches FontTools, it relies on implementation
    details not part of the API, so d9d29b2c53a600bde5ccacfde46d8e6dc77229d1
    banned most version ranges, since the patches could theoretically be
    incompatible with new versions within the range. In practice, though,
    that has never happened, so I am allowing a broad range of versions.
    
    The patches are only applied for specific validated versions. Building
    with other versions in the range should work, but will be slower without
    the patches.

--- font/build.py
+++ font/build.py
@@ -47,6 +47,22 @@ def generate_feature_string(font, lookup):
         return fea_file.read().decode('utf-8')
 
 def patch_fonttools():
+    if fontTools.__version__ not in [
+        '4.2.5',
+        '4.3.0',
+        '4.4.0',
+        '4.4.1',
+        '4.4.2.dev0',
+        '4.4.2',
+        '4.4.3',
+        '4.5.0',
+        '4.6.0',
+        '4.7.0',
+        '4.8.0',
+        '4.8.1',
+    ]:
+        return
+
     getGlyphID_inner = fontTools.ttLib.TTFont.getGlyphID
     def getGlyphID(self, glyphName, requireReal=False):
         try:
