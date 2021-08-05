dscorbett/duployan-font

commit 519392bd2c8951e8f13acb65178fe1fbee83acad

Put primary circles’ cursive points at the top

font/duployan.py
@@ -3961,7 +3961,7 @@ class Builder:
         s_k = Curve(90, 0, clockwise=True, secondary=False)
         j_n = Complex([(1, s_k), (1, n)], maximum_tree_width=1)
         j_n_s = Complex([(3, s_k), (4, n_s)], maximum_tree_width=1)
-        o = Circle(0, 0, clockwise=False)
+        o = Circle(180, 180, clockwise=False)
         o_reverse = Circle(0, 0, clockwise=True, reversed=True)
         ie = Curve(180, 0, clockwise=False)
         short_i = Curve(0, 180, clockwise=True)

tests/circles.test
@@ -13,20 +13,20 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-EC19 1BC41::[uniEC19.reversed_m@420,0|u1BC42.sloan_ow@420,520|_@640,0]
-EC1A 1BC41::[uniEC1A.reversed_n@220,0|u1BC42.sloan_ow@220,520|_@640,0]
+EC19 1BC41::[uniEC19.reversed_m@420,0|u1BC41.a@420,520|_@640,0]
+EC1A 1BC41::[uniEC1A.reversed_n@220,0|u1BC41.a@220,520|_@640,0]
 EC1B 1BC41::[uniEC1B.reversed_j@840,0|u1BC41.a.270p270@120,-100|_@960,0]
 EC1C 1BC41::[uniEC1C.reversed_s@840,0|u1BC41.a.270p270@120,200|_@960,0]
-1BC19 1BC41::[u1BC19.m@420,0|u1BC41.a@420,0|_@640,0]
-1BC1A 1BC41::[u1BC1A.n@220,0|u1BC41.a@220,0|_@640,0]
+1BC19 1BC41::[u1BC19.m@420,0|u1BC41.a.0p0@420,0|_@640,0]
+1BC1A 1BC41::[u1BC1A.n@220,0|u1BC41.a.0p0@220,0|_@640,0]
 1BC1B 1BC41::[u1BC1B.j@120,0|u1BC41.a.90p90@840,-100|_@960,0]
 1BC1C 1BC41::[u1BC1C.s@120,0|u1BC41.a.90p90@840,200|_@960,0]
-1BC41 EC19::[u1BC41.a@420,0|uniEC19.reversed_m@420,0|_@640,0]
-1BC41 EC1A::[u1BC41.a@220,0|uniEC1A.reversed_n@220,0|_@640,0]
+1BC41 EC19::[u1BC41.a.0p0@420,0|uniEC19.reversed_m@420,0|_@640,0]
+1BC41 EC1A::[u1BC41.a.0p0@220,0|uniEC1A.reversed_n@220,0|_@640,0]
 1BC41 EC1B::[u1BC41.a.90p90@840,-100|uniEC1B.reversed_j@840,0|_@960,0]
 1BC41 EC1C::[u1BC41.a.90p90@840,200|uniEC1C.reversed_s@840,0|_@960,0]
-1BC41 1BC19::[u1BC42.sloan_ow@420,520|u1BC19.m@420,0|_@640,0]
-1BC41 1BC1A::[u1BC42.sloan_ow@220,520|u1BC1A.n@220,0|_@640,0]
+1BC41 1BC19::[u1BC41.a@420,520|u1BC19.m@420,0|_@640,0]
+1BC41 1BC1A::[u1BC41.a@220,520|u1BC1A.n@220,0|_@640,0]
 1BC41 1BC1B::[u1BC41.a.270p270@120,-100|u1BC1B.j@120,0|_@960,0]
 1BC41 1BC1C::[u1BC41.a.270p270@120,200|u1BC1C.s@120,0|_@960,0]
 1BC02 1BC41 EC19::[u1BC02.p@320,0|u1BC41.a.270p180@320,-100|uniEC19.reversed_m@420,100|_@640,0]
@@ -48,20 +48,20 @@ EC1C 1BC41::[uniEC1C.reversed_s@840,0|u1BC41.a.270p270@120,200|_@960,0]
 EC19 1BC41 1BC02::[uniEC19.reversed_m@420,0|u1BC41.a.0p270@420,720|u1BC02.p@320,320|_@640,0]
 EC19 1BC41 1BC05::[uniEC19.reversed_m@420,0|u1BC41.a.0n240@420,570|u1BC05.k@507,70|_@640,0]
 EC19 1BC41 EC19::[uniEC19.reversed_m@420,0|u1BC41.a.0n180@420,520|uniEC19.reversed_m@420,520|_@640,0]
-EC19 1BC41 EC1A::[uniEC19.reversed_m@420,0|u1BC42.sloan_ow@420,520|uniEC1A.reversed_n@420,720|_@840,0]
+EC19 1BC41 EC1A::[uniEC19.reversed_m@420,0|u1BC41.a@420,520|uniEC1A.reversed_n@420,720|_@840,0]
 EC19 1BC41 EC1B::[uniEC19.reversed_m@940,0|u1BC41.a.0n90@940,520|uniEC1B.reversed_j@840,620|_@1160,0]
 EC19 1BC41 EC1C::[uniEC19.reversed_m@740,0|u1BC41.a.0n270@740,620|uniEC1C.reversed_s@840,320|_@960,0]
 EC19 1BC41 1BC19::[uniEC19.reversed_m@420,0|u1BC41.a.0p180@420,720|u1BC19.m@420,200|_@640,0]
-EC19 1BC41 1BC1A::[uniEC19.reversed_m@420,0|u1BC42.sloan_ow@420,520|u1BC1A.n@420,0|_@840,0]
+EC19 1BC41 1BC1A::[uniEC19.reversed_m@420,0|u1BC41.a@420,520|u1BC1A.n@420,0|_@840,0]
 EC19 1BC41 1BC1B::[uniEC19.reversed_m@420,0|u1BC41.a.0n90@420,520|u1BC1B.j@320,620|_@1160,0]
 EC19 1BC41 1BC1C::[uniEC19.reversed_m@420,0|u1BC41.a.0p270@420,720|u1BC1C.s@320,520|_@1160,0]
 EC1A 1BC41 1BC02::[uniEC1A.reversed_n@220,0|u1BC41.a.180n270@220,720|u1BC02.p@320,320|_@640,0]
 EC1A 1BC41 1BC05::[uniEC1A.reversed_n@321,0|u1BC41.a.180n240@321,720|u1BC05.k@408,270|_@741,0]
-EC1A 1BC41 EC19::[uniEC1A.reversed_n@420,0|u1BC42.sloan_ow@420,520|uniEC19.reversed_m@420,720|_@840,0]
+EC1A 1BC41 EC19::[uniEC1A.reversed_n@420,0|u1BC41.a@420,520|uniEC19.reversed_m@420,720|_@840,0]
 EC1A 1BC41 EC1A::[uniEC1A.reversed_n@220,0|u1BC41.a.180p0@220,520|uniEC1A.reversed_n@220,520|_@640,0]
 EC1A 1BC41 EC1B::[uniEC1A.reversed_n@740,0|u1BC41.a.180p90@740,520|uniEC1B.reversed_j@840,620|_@1160,0]
 EC1A 1BC41 EC1C::[uniEC1A.reversed_n@740,0|u1BC41.a.180n270@740,720|uniEC1C.reversed_s@840,520|_@1160,0]
-EC1A 1BC41 1BC19::[uniEC1A.reversed_n@420,0|u1BC42.sloan_ow@420,520|u1BC19.m@420,0|_@840,0]
+EC1A 1BC41 1BC19::[uniEC1A.reversed_n@420,0|u1BC41.a@420,520|u1BC19.m@420,0|_@840,0]
 EC1A 1BC41 1BC1A::[uniEC1A.reversed_n@220,0|u1BC41.a.180p0@220,520|u1BC1A.n@220,-200|_@640,0]
 EC1A 1BC41 1BC1B::[uniEC1A.reversed_n@220,0|u1BC41.a.180p90@220,520|u1BC1B.j@320,620|_@1160,0]
 EC1A 1BC41 1BC1C::[uniEC1A.reversed_n@220,0|u1BC41.a.180p270@220,620|u1BC1C.s@120,320|_@960,0]
@@ -88,20 +88,20 @@ EC1C 1BC41 1BC1C::[uniEC1C.reversed_s@1040,0|u1BC41.a.90p270@320,300|u1BC1C.s@12
 1BC19 1BC41 1BC02::[u1BC19.m@420,0|u1BC41.a.0p270@420,0|u1BC02.p@320,-400|_@640,0]
 1BC19 1BC41 1BC05::[u1BC19.m@495,0|u1BC41.a.0p240@495,0|u1BC05.k@408,-350|_@715,0]
 1BC19 1BC41 EC19::[u1BC19.m@420,0|u1BC41.a.0p180@420,0|uniEC19.reversed_m@420,200|_@640,0]
-1BC19 1BC41 EC1A::[u1BC19.m@420,0|u1BC41.a@420,0|uniEC1A.reversed_n@420,0|_@840,0]
+1BC19 1BC41 EC1A::[u1BC19.m@420,0|u1BC41.a.0p0@420,0|uniEC1A.reversed_n@420,0|_@840,0]
 1BC19 1BC41 EC1B::[u1BC19.m@740,0|u1BC41.a.0p90@740,0|uniEC1B.reversed_j@840,100|_@960,0]
 1BC19 1BC41 EC1C::[u1BC19.m@940,0|u1BC41.a.0p270@940,0|uniEC1C.reversed_s@840,-200|_@1160,0]
 1BC19 1BC41 1BC19::[u1BC19.m@420,0|u1BC41.a.0p180@420,0|u1BC19.m@420,-520|_@640,0]
-1BC19 1BC41 1BC1A::[u1BC19.m@420,0|u1BC41.a@420,0|u1BC1A.n@420,-720|_@840,0]
+1BC19 1BC41 1BC1A::[u1BC19.m@420,0|u1BC41.a.0p0@420,0|u1BC1A.n@420,-720|_@840,0]
 1BC19 1BC41 1BC1B::[u1BC19.m@420,0|u1BC41.a.0n90@420,-200|u1BC1B.j@320,-100|_@1160,0]
 1BC19 1BC41 1BC1C::[u1BC19.m@420,0|u1BC41.a.0p270@420,0|u1BC1C.s@320,-200|_@1160,0]
 1BC1A 1BC41 1BC02::[u1BC1A.n@220,0|u1BC41.a.180n270@220,0|u1BC02.p@320,-400|_@640,0]
 1BC1A 1BC41 1BC05::[u1BC1A.n@321,0|u1BC41.a.180n240@321,0|u1BC05.k@408,-450|_@741,0]
-1BC1A 1BC41 EC19::[u1BC1A.n@420,0|u1BC41.a@420,0|uniEC19.reversed_m@420,0|_@840,0]
+1BC1A 1BC41 EC19::[u1BC1A.n@420,0|u1BC41.a.0p0@420,0|uniEC19.reversed_m@420,0|_@840,0]
 1BC1A 1BC41 EC1A::[u1BC1A.n@220,0|u1BC41.a.180n0@220,0|uniEC1A.reversed_n@220,200|_@640,0]
 1BC1A 1BC41 EC1B::[u1BC1A.n@740,0|u1BC41.a.180p90@740,-200|uniEC1B.reversed_j@840,-100|_@1160,0]
 1BC1A 1BC41 EC1C::[u1BC1A.n@740,0|u1BC41.a.180n270@740,0|uniEC1C.reversed_s@840,-200|_@1160,0]
-1BC1A 1BC41 1BC19::[u1BC1A.n@420,0|u1BC41.a@420,0|u1BC19.m@420,-720|_@840,0]
+1BC1A 1BC41 1BC19::[u1BC1A.n@420,0|u1BC41.a.0p0@420,0|u1BC19.m@420,-720|_@840,0]
 1BC1A 1BC41 1BC1A::[u1BC1A.n@220,0|u1BC41.a.180n0@220,0|u1BC1A.n@220,-520|_@640,0]
 1BC1A 1BC41 1BC1B::[u1BC1A.n@220,0|u1BC41.a.180n90@220,0|u1BC1B.j@120,100|_@960,0]
 1BC1A 1BC41 1BC1C::[u1BC1A.n@220,0|u1BC41.a.180n270@220,0|u1BC1C.s@320,-200|_@1160,0]
@@ -126,20 +126,20 @@ EC1C 1BC41 1BC1C::[uniEC1C.reversed_s@1040,0|u1BC41.a.90p270@320,300|u1BC1C.s@12
 1BC1C 1BC41 1BC1B::[u1BC1C.s@120,0|u1BC41.a.90p90@840,200|u1BC1B.j@840,300|_@1680,0]
 1BC1C 1BC41 1BC1C::[u1BC1C.s@120,0|u1BC41.a.90p270@840,300|u1BC1C.s@640,0|_@1480,0]
 
-EC19 1BC42::[uniEC19.reversed_m@420,0|u1BC41.a@420,720|_@640,0]
-EC1A 1BC42::[uniEC1A.reversed_n@220,0|u1BC41.a@220,720|_@640,0]
+EC19 1BC42::[uniEC19.reversed_m@420,0|u1BC41.a.0p0@420,720|_@640,0]
+EC1A 1BC42::[uniEC1A.reversed_n@220,0|u1BC41.a.0p0@220,720|_@640,0]
 EC1B 1BC42::[uniEC1B.reversed_j@1040,0|u1BC41.a.90p90@320,-100|_@1160,0]
 EC1C 1BC42::[uniEC1C.reversed_s@1040,0|u1BC41.a.90p90@320,200|_@1160,0]
-1BC19 1BC42::[u1BC19.m@420,0|u1BC42.sloan_ow@420,-200|_@640,0]
-1BC1A 1BC42::[u1BC1A.n@220,0|u1BC42.sloan_ow@220,-200|_@640,0]
+1BC19 1BC42::[u1BC19.m@420,0|u1BC41.a@420,-200|_@640,0]
+1BC1A 1BC42::[u1BC1A.n@220,0|u1BC41.a@220,-200|_@640,0]
 1BC1B 1BC42::[u1BC1B.j@120,0|u1BC41.a.270p270@840,-100|_@1160,0]
 1BC1C 1BC42::[u1BC1C.s@120,0|u1BC41.a.270p270@840,200|_@1160,0]
-1BC42 EC19::[u1BC42.sloan_ow@420,-200|uniEC19.reversed_m@420,0|_@640,0]
-1BC42 EC1A::[u1BC42.sloan_ow@220,-200|uniEC1A.reversed_n@220,0|_@640,0]
+1BC42 EC19::[u1BC41.a@420,-200|uniEC19.reversed_m@420,0|_@640,0]
+1BC42 EC1A::[u1BC41.a@220,-200|uniEC1A.reversed_n@220,0|_@640,0]
 1BC42 EC1B::[u1BC41.a.270p270@840,-100|uniEC1B.reversed_j@840,0|_@1160,0]
 1BC42 EC1C::[u1BC41.a.270p270@840,200|uniEC1C.reversed_s@840,0|_@1160,0]
-1BC42 1BC19::[u1BC41.a@420,720|u1BC19.m@420,0|_@640,0]
-1BC42 1BC1A::[u1BC41.a@220,720|u1BC1A.n@220,0|_@640,0]
+1BC42 1BC19::[u1BC41.a.0p0@420,720|u1BC19.m@420,0|_@640,0]
+1BC42 1BC1A::[u1BC41.a.0p0@220,720|u1BC1A.n@220,0|_@640,0]
 1BC42 1BC1B::[u1BC41.a.90p90@320,-100|u1BC1B.j@320,0|_@1160,0]
 1BC42 1BC1C::[u1BC41.a.90p90@320,200|u1BC1C.s@320,0|_@1160,0]
 1BC02 1BC42 EC19::[u1BC02.p@420,0|u1BC42.sloan_ow.270n0r@420,-100|uniEC19.reversed_m@420,100|_@540,0]
@@ -160,22 +160,22 @@ EC1C 1BC42::[uniEC1C.reversed_s@1040,0|u1BC41.a.90p90@320,200|_@1160,0]
 1BC05 1BC42 1BC1C::[u1BC05.k@421,0|u1BC42.sloan_ow.240p90r@133,-150|u1BC1C.s@320,-250|_@1160,0]
 EC19 1BC42 1BC02::[uniEC19.reversed_m@420,0|u1BC42.sloan_ow.0n90r@420,520|u1BC02.p@320,220|_@640,0]
 EC19 1BC42 1BC05::[uniEC19.reversed_m@545,0|u1BC42.sloan_ow.0p240r@545,720|u1BC05.k@408,283|_@765,0]
-EC19 1BC42 EC19::[uniEC19.reversed_m@420,0|u1BC41.a@420,720|uniEC19.reversed_m@420,720|_@640,0]
-EC19 1BC42 EC1A::[uniEC19.reversed_m@420,0|u1BC41.a@420,720|uniEC1A.reversed_n@420,720|_@840,0]
+EC19 1BC42 EC19::[uniEC19.reversed_m@420,0|u1BC41.a.0p0@420,720|uniEC19.reversed_m@420,720|_@640,0]
+EC19 1BC42 EC1A::[uniEC19.reversed_m@420,0|u1BC41.a.0p0@420,720|uniEC1A.reversed_n@420,720|_@840,0]
 EC19 1BC42 EC1B::[uniEC19.reversed_m@940,0|u1BC42.sloan_ow.0p270r@940,720|uniEC1B.reversed_j@840,720|_@1160,0]
 EC19 1BC42 EC1C::[uniEC19.reversed_m@940,0|u1BC42.sloan_ow.0p270r@940,720|uniEC1C.reversed_s@840,420|_@1160,0]
-EC19 1BC42 1BC19::[uniEC19.reversed_m@420,0|u1BC42.sloan_ow@420,520|u1BC19.m@420,0|_@640,0]
-EC19 1BC42 1BC1A::[uniEC19.reversed_m@420,0|u1BC41.a@420,720|u1BC1A.n@420,0|_@840,0]
+EC19 1BC42 1BC19::[uniEC19.reversed_m@420,0|u1BC41.a@420,520|u1BC19.m@420,0|_@640,0]
+EC19 1BC42 1BC1A::[uniEC19.reversed_m@420,0|u1BC41.a.0p0@420,720|u1BC1A.n@420,0|_@840,0]
 EC19 1BC42 1BC1B::[uniEC19.reversed_m@420,0|u1BC42.sloan_ow.0p270r@420,720|u1BC1B.j@320,720|_@1160,0]
 EC19 1BC42 1BC1C::[uniEC19.reversed_m@420,0|u1BC42.sloan_ow.0n90r@420,520|u1BC1C.s@320,420|_@1160,0]
 EC1A 1BC42 1BC02::[uniEC1A.reversed_n@220,0|u1BC42.sloan_ow.180p90r@220,520|u1BC02.p@320,220|_@640,0]
 EC1A 1BC42 1BC05::[uniEC1A.reversed_n@271,0|u1BC42.sloan_ow.180p60r@271,520|u1BC05.k@408,157|_@691,0]
-EC1A 1BC42 EC19::[uniEC1A.reversed_n@420,0|u1BC41.a@420,720|uniEC19.reversed_m@420,720|_@840,0]
-EC1A 1BC42 EC1A::[uniEC1A.reversed_n@220,0|u1BC41.a@220,720|uniEC1A.reversed_n@220,720|_@640,0]
+EC1A 1BC42 EC19::[uniEC1A.reversed_n@420,0|u1BC41.a.0p0@420,720|uniEC19.reversed_m@420,720|_@840,0]
+EC1A 1BC42 EC1A::[uniEC1A.reversed_n@220,0|u1BC41.a.0p0@220,720|uniEC1A.reversed_n@220,720|_@640,0]
 EC1A 1BC42 EC1B::[uniEC1A.reversed_n@740,0|u1BC42.sloan_ow.180n270r@740,720|uniEC1B.reversed_j@840,720|_@1160,0]
 EC1A 1BC42 EC1C::[uniEC1A.reversed_n@740,0|u1BC42.sloan_ow.180p90r@740,520|uniEC1C.reversed_s@840,420|_@1160,0]
-EC1A 1BC42 1BC19::[uniEC1A.reversed_n@420,0|u1BC41.a@420,720|u1BC19.m@420,0|_@840,0]
-EC1A 1BC42 1BC1A::[uniEC1A.reversed_n@220,0|u1BC41.a@220,720|u1BC1A.n@220,0|_@640,0]
+EC1A 1BC42 1BC19::[uniEC1A.reversed_n@420,0|u1BC41.a.0p0@420,720|u1BC19.m@420,0|_@840,0]
+EC1A 1BC42 1BC1A::[uniEC1A.reversed_n@220,0|u1BC41.a.0p0@220,720|u1BC1A.n@220,0|_@640,0]
 EC1A 1BC42 1BC1B::[uniEC1A.reversed_n@220,0|u1BC42.sloan_ow.180n270r@220,720|u1BC1B.j@320,720|_@1160,0]
 EC1A 1BC42 1BC1C::[uniEC1A.reversed_n@220,0|u1BC42.sloan_ow.180n270r@220,720|u1BC1C.s@320,420|_@1160,0]
 EC1B 1BC42 1BC02::[uniEC1B.reversed_j@1040,0|u1BC41.a.90p90@320,-100|u1BC02.p@320,-500|_@1160,0]
@@ -200,22 +200,22 @@ EC1C 1BC42 1BC1B::[uniEC1C.reversed_s@1040,0|u1BC41.a.90p90@320,200|u1BC1B.j@320
 EC1C 1BC42 1BC1C::[uniEC1C.reversed_s@840,0|u1BC41.a.270p270@120,200|u1BC1C.s@120,0|_@960,0]
 1BC19 1BC42 1BC02::[u1BC19.m@420,0|u1BC42.sloan_ow.0n90r@420,-200|u1BC02.p@320,-500|_@640,0]
 1BC19 1BC42 1BC05::[u1BC19.m@466,0|u1BC42.sloan_ow.0n60r@466,-200|u1BC05.k@408,-500|_@686,0]
-1BC19 1BC42 EC19::[u1BC19.m@420,0|u1BC42.sloan_ow@420,-200|uniEC19.reversed_m@420,0|_@640,0]
-1BC19 1BC42 EC1A::[u1BC19.m@420,0|u1BC42.sloan_ow@420,-200|uniEC1A.reversed_n@420,0|_@840,0]
+1BC19 1BC42 EC19::[u1BC19.m@420,0|u1BC41.a@420,-200|uniEC19.reversed_m@420,0|_@640,0]
+1BC19 1BC42 EC1A::[u1BC19.m@420,0|u1BC41.a@420,-200|uniEC1A.reversed_n@420,0|_@840,0]
 1BC19 1BC42 EC1B::[u1BC19.m@940,0|u1BC42.sloan_ow.0n90r@940,-200|uniEC1B.reversed_j@840,0|_@1160,0]
 1BC19 1BC42 EC1C::[u1BC19.m@940,0|u1BC42.sloan_ow.0n90r@940,-200|uniEC1C.reversed_s@840,-300|_@1160,0]
-1BC19 1BC42 1BC19::[u1BC19.m@420,0|u1BC42.sloan_ow@420,-200|u1BC19.m@420,-720|_@640,0]
-1BC19 1BC42 1BC1A::[u1BC19.m@420,0|u1BC42.sloan_ow@420,-200|u1BC1A.n@420,-720|_@840,0]
+1BC19 1BC42 1BC19::[u1BC19.m@420,0|u1BC41.a@420,-200|u1BC19.m@420,-720|_@640,0]
+1BC19 1BC42 1BC1A::[u1BC19.m@420,0|u1BC41.a@420,-200|u1BC1A.n@420,-720|_@840,0]
 1BC19 1BC42 1BC1B::[u1BC19.m@420,0|u1BC42.sloan_ow.0p270r@420,0|u1BC1B.j@320,0|_@1160,0]
 1BC19 1BC42 1BC1C::[u1BC19.m@420,0|u1BC42.sloan_ow.0n90r@420,-200|u1BC1C.s@320,-300|_@1160,0]
 1BC1A 1BC42 1BC02::[u1BC1A.n@220,0|u1BC42.sloan_ow.180p90r@220,-200|u1BC02.p@320,-500|_@640,0]
 1BC1A 1BC42 1BC05::[u1BC1A.n@271,0|u1BC42.sloan_ow.180p60r@271,-200|u1BC05.k@408,-563|_@691,0]
-1BC1A 1BC42 EC19::[u1BC1A.n@420,0|u1BC42.sloan_ow@420,-200|uniEC19.reversed_m@420,0|_@840,0]
-1BC1A 1BC42 EC1A::[u1BC1A.n@220,0|u1BC42.sloan_ow@220,-200|uniEC1A.reversed_n@220,0|_@640,0]
+1BC1A 1BC42 EC19::[u1BC1A.n@420,0|u1BC41.a@420,-200|uniEC19.reversed_m@420,0|_@840,0]
+1BC1A 1BC42 EC1A::[u1BC1A.n@220,0|u1BC41.a@220,-200|uniEC1A.reversed_n@220,0|_@640,0]
 1BC1A 1BC42 EC1B::[u1BC1A.n@740,0|u1BC42.sloan_ow.180n270r@740,0|uniEC1B.reversed_j@840,0|_@1160,0]
 1BC1A 1BC42 EC1C::[u1BC1A.n@740,0|u1BC42.sloan_ow.180p90r@740,-200|uniEC1C.reversed_s@840,-300|_@1160,0]
-1BC1A 1BC42 1BC19::[u1BC1A.n@420,0|u1BC42.sloan_ow@420,-200|u1BC19.m@420,-720|_@840,0]
-1BC1A 1BC42 1BC1A::[u1BC1A.n@220,0|u1BC42.sloan_ow@220,-200|u1BC1A.n@220,-720|_@640,0]
+1BC1A 1BC42 1BC19::[u1BC1A.n@420,0|u1BC41.a@420,-200|u1BC19.m@420,-720|_@840,0]
+1BC1A 1BC42 1BC1A::[u1BC1A.n@220,0|u1BC41.a@220,-200|u1BC1A.n@220,-720|_@640,0]
 1BC1A 1BC42 1BC1B::[u1BC1A.n@220,0|u1BC42.sloan_ow.180p90r@220,-200|u1BC1B.j@320,0|_@1160,0]
 1BC1A 1BC42 1BC1C::[u1BC1A.n@220,0|u1BC42.sloan_ow.180p90r@220,-200|u1BC1C.s@320,-300|_@1160,0]
 1BC1B 1BC42 1BC02::[u1BC1B.j@120,0|u1BC41.a.270p270@840,-100|u1BC02.p@840,-500|_@1160,0]

tests/orienting-sequences.test
@@ -109,10 +109,10 @@
 1BC03 1BC52 1BC52 1BC02::[u1BC03.t@120,800|u1BC51.u.0n270@620,650|u1BC51.u@770,500|u1BC02.p@920,0|_@1040,0]
 
 # Circle and semicircle, isolated
-1BC41 1BC46::[u1BC41.a.0p0.diph1@120,0|u1BC46.i.180p0.diph2@120,-200|_@440,0]
+1BC41 1BC46::[u1BC41.a.180p180.diph1@320,0|u1BC46.i.0p180.diph2@320,200|_@440,0]
 1BC42 1BC46::[u1BC41.a.0n0.diph1@120,0|u1BC46.i.180n0.diph2@120,200|_@440,0]
-1BC41 1BC47::[u1BC41.a@220,0|u1BC46.i.0n180@220,-200|_@440,0]
-1BC42 1BC47::[u1BC42.sloan_ow@220,0|u1BC46.i.0p180@220,200|_@440,0]
+1BC41 1BC47::[u1BC41.a@220,0|u1BC46.i.180n0@220,200|_@440,0]
+1BC42 1BC47::[u1BC41.a@220,0|u1BC46.i.0p180@220,200|_@440,0]
 1BC41 1BC4A::[u1BC41.a.90n270.circle@120,0|u1BC4A.ui@320,100|_@640,0]
 1BC42 1BC4A::[u1BC41.a.90n270.circle@120,0|u1BC4A.ui@320,100|_@640,0]
 
@@ -139,7 +139,7 @@
 1BC06 1BC42 1BC46 1BC03::[u1BC06.l@120,0|u1BC41.a.45n45.diph1@620,329|u1BC46.i.225n45.diph2@549,429|u1BC03.t@478,641|_@1098,0]
 1BC02 1BC41 1BC47 1BC02::[u1BC02.p@320,0|u1BC41.a.270p270@320,-100|u1BC46.i.270n90@320,-100|u1BC02.p@120,-500|_@640,0]
 1BC02 1BC42 1BC47 1BC02::[u1BC02.p@320,0|u1BC41.a.90p90@320,-100|u1BC47.e@320,-100|u1BC02.p@520,-500|_@640,0]
-1BC03 1BC41 1BC47 1BC02::[u1BC03.t@120,700|u1BC41.a@620,700|u1BC46.i.0n180@620,500|u1BC02.p@620,0|_@840,0]
+1BC03 1BC41 1BC47 1BC02::[u1BC03.t@120,700|u1BC41.a.0p0@620,700|u1BC46.i.0n180@620,500|u1BC02.p@620,0|_@840,0]
 1BC03 1BC42 1BC47 1BC02::[u1BC03.t@120,500|u1BC42.sloan_ow.0n90r@620,300|u1BC46.i.90p270@520,500|u1BC02.p@320,0|_@840,0]
 1BC04 1BC42 1BC47 1BC03::[u1BC04.f@120,0|u1BC42.sloan_ow.300p180r@408,-50|u1BC46.i.180n0@395,150|u1BC03.t@395,350|_@1015,0]
 1BC06 1BC42 1BC47 1BC03::[u1BC06.l@120,0|u1BC42.sloan_ow.45n180r@620,329|u1BC46.i@591,129|u1BC03.t@591,129|_@1211,0]

tests/width.test
@@ -32,4 +32,4 @@
 
 # If HarfBuzz’s `HB_BUFFER_MAX_OPS_FACTOR` is too low, later lookups
 # will not be applied.
-1BC44 1BC03 1BC04 1BC44 1BC05 1BC04 1BC02::[u1BC44.o@320,500|u1BC03.t@320,500|u1BC04.f@820,0|u1BC44.o.300p240@1108,-100|u1BC05.k@1108,-300|u1BC04.f@820,-800|u1BC02.p@1108,-1300|_@1601,0]
+1BC44 1BC03 1BC04 1BC44 1BC05 1BC04 1BC02::[u1BC44.o.0p0@320,500|u1BC03.t@320,500|u1BC04.f@820,0|u1BC44.o.300p240@1108,-100|u1BC05.k@1108,-300|u1BC04.f@820,-800|u1BC02.p@1108,-1300|_@1601,0]

commit 2e2dcff3fd342ed391b7ca6329c80c742bf35320

Fix U+1BC85 HIGH CIRCLE and U+1BC9C O WITH CROSS
They were broken by 519392bd2c8951e8f13acb65178fe1fbee83acad.

font/duployan.py
@@ -4017,7 +4017,7 @@ class Builder:
         high_grave = Complex(separate_affix_guideline + [(1.5 * stroke_gap + light_line, Space(90, margins=False)), (1, Line(0), True), (0.5, Line(135, stretchy=False))])
         high_long_grave = Complex(separate_affix_guideline + [(1.5 * stroke_gap + light_line, Space(90, margins=False)), (1.25, Line(0), True), (0.75, Line(180, stretchy=False)), (0.4, Line(120, stretchy=False))])
         high_dot = Complex(separate_affix_guideline + [(1.5 * stroke_gap + light_line, Space(90, margins=False)), (0.75, Line(0), True), (1, Dot(centered=True))])
-        high_circle = Complex(separate_affix_guideline + [(1.5 * stroke_gap + light_line, Space(90, margins=False)), (0.75, Line(0), True), (2, o)])
+        high_circle = Complex(separate_affix_guideline + [(1.5 * stroke_gap + light_line, Space(90, margins=False)), (0.75, Line(0), True), (2, Circle(0, 0, clockwise=False))])
         high_line = Complex(separate_affix_guideline + [(1.5 * stroke_gap + light_line, Space(90, margins=False)), (0.5, Line(0), True), (0.5, Line(0, stretchy=False))])
         high_wave = Complex(separate_affix_guideline + [(1.5 * stroke_gap + light_line, Space(90, margins=False)), (0.375, Line(0), True), (2, Curve(90, 315, clockwise=True)), (RADIUS * math.sqrt(2) / 500, Line(315, stretchy=False)), (2, Curve(315, 90, clockwise=False))])
         high_vertical = Complex(separate_affix_guideline + [(1.5 * stroke_gap + light_line, Space(90, margins=False)), (0.75, Line(0), True), (0.5, Line(90, stretchy=False))])
@@ -4031,7 +4031,7 @@ class Builder:
         low_wave = Complex(separate_affix_guideline + [(1.5 * stroke_gap, Space(270, margins=False)), (0.375, Line(0), True), (100, Space(180, margins=False)), (2, Curve(180, 90, clockwise=False), True), (2, Curve(90, 315, clockwise=True)), (RADIUS * math.sqrt(2) / 500, Line(315, stretchy=False)), (2, Curve(315, 90, clockwise=False))])
         low_vertical = Complex(separate_affix_guideline + [(1.5 * stroke_gap, Space(270, margins=False)), (0.75, Line(0), True), (0.5, Line(270, stretchy=False))])
         low_arrow = Complex(separate_affix_guideline + [(1.5 * stroke_gap, Space(270, margins=False)), (0.55, Line(0), True), (0.4, Line(0, stretchy=False)), (0.4, Line(240, stretchy=False))])
-        likalisti = Complex([(5, o), (375, Space(90, margins=False)), (0.5, p), (math.hypot(125, 125), Space(135, margins=False)), (0.5, Line(0, stretchy=False))])
+        likalisti = Complex([(5, Circle(0, 0, clockwise=False)), (375, Space(90, margins=False)), (0.5, p), (math.hypot(125, 125), Space(135, margins=False)), (0.5, Line(0, stretchy=False))])
         dotted_square = [(152, Space(270, margins=False)), (0.26 - light_line / 1000, Line(90, stretchy=False)), (58 + light_line, Space(90, margins=False)), (0.264 - light_line / 500, Line(90, stretchy=False)), (58 + light_line, Space(90, margins=False)), (0.264 - light_line / 500, Line(90, stretchy=False)), (58 + light_line, Space(90, margins=False)), (0.264 - light_line / 500, Line(90, stretchy=False)), (58 + light_line, Space(90, margins=False)), (0.26 - light_line / 1000, Line(90, stretchy=False)), (0.26 - light_line / 1000, Line(0, stretchy=False)), (58 + light_line, Space(0, margins=False)), (0.264 - light_line / 500, Line(0, stretchy=False)), (58 + light_line, Space(0, margins=False)), (0.264 - light_line / 500, Line(0, stretchy=False)), (58 + light_line, Space(0, margins=False)), (0.264 - light_line / 500, Line(0, stretchy=False)), (58 + light_line, Space(0, margins=False)), (0.26 - light_line / 1000, Line(0, stretchy=False)), (0.26 - light_line / 1000, Line(270, stretchy=False)), (58 + light_line, Space(270, margins=False)), (0.264 - light_line / 500, Line(270, stretchy=False)), (58 + light_line, Space(270, margins=False)), (0.264 - light_line / 500, Line(270, stretchy=False)), (58 + light_line, Space(270, margins=False)), (0.264 - light_line / 500, Line(270, stretchy=False)), (58 + light_line, Space(270, margins=False)), (0.26 - light_line / 1000, Line(270, stretchy=False)), (0.26 - light_line / 1000, Line(180, stretchy=False)), (58 + light_line, Space(180, margins=False)), (0.264 - light_line / 500, Line(180, stretchy=False)), (58 + light_line, Space(180, margins=False)), (0.264 - light_line / 500, Line(180, stretchy=False)), (58 + light_line, Space(180, margins=False)), (0.264 - light_line / 500, Line(180, stretchy=False)), (58 + light_line, Space(180, margins=False)), (0.26 - light_line / 1000, Line(180, stretchy=False))]
         dtls = InvalidDTLS(instructions=dotted_square + [(341, Space(0, margins=False)), (173, Space(90, margins=False)), (0.238, Line(180, stretchy=False)), (0.412, Line(90, stretchy=False)), (130, Space(90, margins=False)), (0.412, Line(90, stretchy=False)), (0.18, Line(0, stretchy=False)), (2.06, Curve(0, 180, clockwise=True, stretch=-27 / 115, long=True, relative_stretch=False)), (0.18, Line(180, stretchy=False)), (369, Space(0, margins=False)), (0.412, Line(90, stretchy=False)), (0.148, Line(180, stretchy=False), True), (0.296, Line(0, stretchy=False)), (341, Space(270, margins=False)), (14.5, Space(180, margins=False)), (.345 * 2.58, Curve(164, 196, clockwise=False, stretch=2.058, long=True, relative_stretch=False)), (.345 * 2.88, Curve(196, 341, clockwise=False, stretch=0.25, long=True, relative_stretch=False)), (.345 *0.224, Line(341, stretchy=False)), (.345 * 2.88, Curve(341, 196, clockwise=True, stretch=0.25, long=True, relative_stretch=False)), (.345 * 2.58, Curve(196, 164, clockwise=True, stretch=2.058, long=True, relative_stretch=False))])
         chinook_period = Complex([(100, Space(90, margins=False)), (1, Line(0, stretchy=False)), (179, Space(90, margins=False)), (1, Line(180, stretchy=False))])