dscorbett/duployan-font

commit df2d59d7e8de4c6f142c39f6c454068c64fb82f3

Tweak schema groups to fix pseudo-cursives

font/duployan.py
@@ -2992,8 +2992,8 @@ class Schema:
             self.widthless,
             tuple(m.group for m in self.marks or []),
             self.glyph_class,
-            self.context_in == NO_CONTEXT and self.diphthong_1 and isinstance(self.path, Circle),
-            self.context_out == NO_CONTEXT and self.diphthong_2 and isinstance(self.path, Circle),
+            self.context_in == NO_CONTEXT and not self.diphthong_1,
+            self.context_out == NO_CONTEXT and not self.diphthong_2,
             self.diphthong_1,
             self.diphthong_2,
         )

tests/circles.test
@@ -13,8 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-EC19 1BC41::[uniEC19.reversed_m@420,0|u1BC41.a@420,520|_@640,0]
-EC1A 1BC41::[uniEC1A.reversed_n@220,0|u1BC41.a@220,520|_@640,0]
+EC19 1BC41::[uniEC19.reversed_m@420,0|u1BC41.a.0n0@420,520|_@640,0]
+EC1A 1BC41::[uniEC1A.reversed_n@220,0|u1BC41.a.0n0@220,520|_@640,0]
 EC1B 1BC41::[uniEC1B.reversed_j@840,0|u1BC41.a.90n90@120,-100|_@960,0]
 EC1C 1BC41::[uniEC1C.reversed_s@840,0|u1BC41.a.90n90@120,200|_@960,0]
 1BC19 1BC41::[u1BC19.m@420,0|u1BC41.a.0p0@420,0|_@640,0]
@@ -25,8 +25,8 @@ EC1C 1BC41::[uniEC1C.reversed_s@840,0|u1BC41.a.90n90@120,200|_@960,0]
 1BC41 EC1A::[u1BC41.a.0p0@220,0|uniEC1A.reversed_n@220,0|_@640,0]
 1BC41 EC1B::[u1BC41.a.90p90@840,-100|uniEC1B.reversed_j@840,0|_@960,0]
 1BC41 EC1C::[u1BC41.a.90p90@840,200|uniEC1C.reversed_s@840,0|_@960,0]
-1BC41 1BC19::[u1BC41.a@420,520|u1BC19.m@420,0|_@640,0]
-1BC41 1BC1A::[u1BC41.a@220,520|u1BC1A.n@220,0|_@640,0]
+1BC41 1BC19::[u1BC41.a.0n0@420,520|u1BC19.m@420,0|_@640,0]
+1BC41 1BC1A::[u1BC41.a.0n0@220,520|u1BC1A.n@220,0|_@640,0]
 1BC41 1BC1B::[u1BC41.a.90n90@120,-100|u1BC1B.j@120,0|_@960,0]
 1BC41 1BC1C::[u1BC41.a.90n90@120,200|u1BC1C.s@120,0|_@960,0]
 1BC02 1BC41 EC19::[u1BC02.p@320,0|u1BC41.a.270p180@320,-100|uniEC19.reversed_m@420,100|_@640,0]
@@ -48,20 +48,20 @@ EC1C 1BC41::[uniEC1C.reversed_s@840,0|u1BC41.a.90n90@120,200|_@960,0]
 EC19 1BC41 1BC02::[uniEC19.reversed_m@420,0|u1BC41.a.0p270@420,720|u1BC02.p@320,320|_@640,0]
 EC19 1BC41 1BC05::[uniEC19.reversed_m@420,0|u1BC41.a.0n240@420,570|u1BC05.k@507,70|_@640,0]
 EC19 1BC41 EC19::[uniEC19.reversed_m@420,0|u1BC41.a.0n180@420,520|uniEC19.reversed_m@420,520|_@640,0]
-EC19 1BC41 EC1A::[uniEC19.reversed_m@420,0|u1BC41.a@420,520|uniEC1A.reversed_n@420,720|_@840,0]
+EC19 1BC41 EC1A::[uniEC19.reversed_m@420,0|u1BC41.a.0n0@420,520|uniEC1A.reversed_n@420,720|_@840,0]
 EC19 1BC41 EC1B::[uniEC19.reversed_m@940,0|u1BC41.a.0n90@940,520|uniEC1B.reversed_j@840,620|_@1160,0]
 EC19 1BC41 EC1C::[uniEC19.reversed_m@740,0|u1BC41.a.0n270@740,620|uniEC1C.reversed_s@840,320|_@960,0]
 EC19 1BC41 1BC19::[uniEC19.reversed_m@420,0|u1BC41.a.0p180@420,720|u1BC19.m@420,200|_@640,0]
-EC19 1BC41 1BC1A::[uniEC19.reversed_m@420,0|u1BC41.a@420,520|u1BC1A.n@420,0|_@840,0]
+EC19 1BC41 1BC1A::[uniEC19.reversed_m@420,0|u1BC41.a.0n0@420,520|u1BC1A.n@420,0|_@840,0]
 EC19 1BC41 1BC1B::[uniEC19.reversed_m@420,0|u1BC41.a.0n90@420,520|u1BC1B.j@320,620|_@1160,0]
 EC19 1BC41 1BC1C::[uniEC19.reversed_m@420,0|u1BC41.a.0p270@420,720|u1BC1C.s@320,520|_@1160,0]
 EC1A 1BC41 1BC02::[uniEC1A.reversed_n@220,0|u1BC41.a.180n270@220,720|u1BC02.p@320,320|_@640,0]
 EC1A 1BC41 1BC05::[uniEC1A.reversed_n@321,0|u1BC41.a.180n240@321,720|u1BC05.k@408,270|_@741,0]
-EC1A 1BC41 EC19::[uniEC1A.reversed_n@420,0|u1BC41.a@420,520|uniEC19.reversed_m@420,720|_@840,0]
+EC1A 1BC41 EC19::[uniEC1A.reversed_n@420,0|u1BC41.a.0n0@420,520|uniEC19.reversed_m@420,720|_@840,0]
 EC1A 1BC41 EC1A::[uniEC1A.reversed_n@220,0|u1BC41.a.180p0@220,520|uniEC1A.reversed_n@220,520|_@640,0]
 EC1A 1BC41 EC1B::[uniEC1A.reversed_n@740,0|u1BC41.a.180p90@740,520|uniEC1B.reversed_j@840,620|_@1160,0]
 EC1A 1BC41 EC1C::[uniEC1A.reversed_n@740,0|u1BC41.a.180n270@740,720|uniEC1C.reversed_s@840,520|_@1160,0]
-EC1A 1BC41 1BC19::[uniEC1A.reversed_n@420,0|u1BC41.a@420,520|u1BC19.m@420,0|_@840,0]
+EC1A 1BC41 1BC19::[uniEC1A.reversed_n@420,0|u1BC41.a.0n0@420,520|u1BC19.m@420,0|_@840,0]
 EC1A 1BC41 1BC1A::[uniEC1A.reversed_n@220,0|u1BC41.a.180p0@220,520|u1BC1A.n@220,-200|_@640,0]
 EC1A 1BC41 1BC1B::[uniEC1A.reversed_n@220,0|u1BC41.a.180p90@220,520|u1BC1B.j@320,620|_@1160,0]
 EC1A 1BC41 1BC1C::[uniEC1A.reversed_n@220,0|u1BC41.a.180p270@220,620|u1BC1C.s@120,320|_@960,0]
@@ -130,12 +130,12 @@ EC19 1BC42::[uniEC19.reversed_m@420,0|u1BC41.a.0p0@420,720|_@640,0]
 EC1A 1BC42::[uniEC1A.reversed_n@220,0|u1BC41.a.0p0@220,720|_@640,0]
 EC1B 1BC42::[uniEC1B.reversed_j@1040,0|u1BC41.a.90p90@320,-100|_@1160,0]
 EC1C 1BC42::[uniEC1C.reversed_s@1040,0|u1BC41.a.90p90@320,200|_@1160,0]
-1BC19 1BC42::[u1BC19.m@420,0|u1BC41.a@420,-200|_@640,0]
-1BC1A 1BC42::[u1BC1A.n@220,0|u1BC41.a@220,-200|_@640,0]
+1BC19 1BC42::[u1BC19.m@420,0|u1BC41.a.0n0@420,-200|_@640,0]
+1BC1A 1BC42::[u1BC1A.n@220,0|u1BC41.a.0n0@220,-200|_@640,0]
 1BC1B 1BC42::[u1BC1B.j@120,0|u1BC41.a.90n90@840,-100|_@1160,0]
 1BC1C 1BC42::[u1BC1C.s@120,0|u1BC41.a.90n90@840,200|_@1160,0]
-1BC42 EC19::[u1BC41.a@420,-200|uniEC19.reversed_m@420,0|_@640,0]
-1BC42 EC1A::[u1BC41.a@220,-200|uniEC1A.reversed_n@220,0|_@640,0]
+1BC42 EC19::[u1BC41.a.0n0@420,-200|uniEC19.reversed_m@420,0|_@640,0]
+1BC42 EC1A::[u1BC41.a.0n0@220,-200|uniEC1A.reversed_n@220,0|_@640,0]
 1BC42 EC1B::[u1BC41.a.90n90@840,-100|uniEC1B.reversed_j@840,0|_@1160,0]
 1BC42 EC1C::[u1BC41.a.90n90@840,200|uniEC1C.reversed_s@840,0|_@1160,0]
 1BC42 1BC19::[u1BC41.a.0p0@420,720|u1BC19.m@420,0|_@640,0]
@@ -164,7 +164,7 @@ EC19 1BC42 EC19::[uniEC19.reversed_m@420,0|u1BC41.a.0p0@420,720|uniEC19.reversed
 EC19 1BC42 EC1A::[uniEC19.reversed_m@420,0|u1BC41.a.0p0@420,720|uniEC1A.reversed_n@420,720|_@840,0]
 EC19 1BC42 EC1B::[uniEC19.reversed_m@940,0|u1BC42.sloan_ow.0p270r@940,720|uniEC1B.reversed_j@840,720|_@1160,0]
 EC19 1BC42 EC1C::[uniEC19.reversed_m@940,0|u1BC42.sloan_ow.0p270r@940,720|uniEC1C.reversed_s@840,420|_@1160,0]
-EC19 1BC42 1BC19::[uniEC19.reversed_m@420,0|u1BC41.a@420,520|u1BC19.m@420,0|_@640,0]
+EC19 1BC42 1BC19::[uniEC19.reversed_m@420,0|u1BC41.a.0n0@420,520|u1BC19.m@420,0|_@640,0]
 EC19 1BC42 1BC1A::[uniEC19.reversed_m@420,0|u1BC41.a.0p0@420,720|u1BC1A.n@420,0|_@840,0]
 EC19 1BC42 1BC1B::[uniEC19.reversed_m@420,0|u1BC42.sloan_ow.0p270r@420,720|u1BC1B.j@320,720|_@1160,0]
 EC19 1BC42 1BC1C::[uniEC19.reversed_m@420,0|u1BC42.sloan_ow.0n90r@420,520|u1BC1C.s@320,420|_@1160,0]
@@ -200,22 +200,22 @@ EC1C 1BC42 1BC1B::[uniEC1C.reversed_s@1040,0|u1BC41.a.90p90@320,200|u1BC1B.j@320
 EC1C 1BC42 1BC1C::[uniEC1C.reversed_s@840,0|u1BC41.a.90n90@120,200|u1BC1C.s@120,0|_@960,0]
 1BC19 1BC42 1BC02::[u1BC19.m@420,0|u1BC42.sloan_ow.0n90r@420,-200|u1BC02.p@320,-500|_@640,0]
 1BC19 1BC42 1BC05::[u1BC19.m@466,0|u1BC42.sloan_ow.0n60r@466,-200|u1BC05.k@408,-500|_@686,0]
-1BC19 1BC42 EC19::[u1BC19.m@420,0|u1BC41.a@420,-200|uniEC19.reversed_m@420,0|_@640,0]
-1BC19 1BC42 EC1A::[u1BC19.m@420,0|u1BC41.a@420,-200|uniEC1A.reversed_n@420,0|_@840,0]
+1BC19 1BC42 EC19::[u1BC19.m@420,0|u1BC41.a.0n0@420,-200|uniEC19.reversed_m@420,0|_@640,0]
+1BC19 1BC42 EC1A::[u1BC19.m@420,0|u1BC41.a.0n0@420,-200|uniEC1A.reversed_n@420,0|_@840,0]
 1BC19 1BC42 EC1B::[u1BC19.m@940,0|u1BC42.sloan_ow.0n90r@940,-200|uniEC1B.reversed_j@840,0|_@1160,0]
 1BC19 1BC42 EC1C::[u1BC19.m@940,0|u1BC42.sloan_ow.0n90r@940,-200|uniEC1C.reversed_s@840,-300|_@1160,0]
-1BC19 1BC42 1BC19::[u1BC19.m@420,0|u1BC41.a@420,-200|u1BC19.m@420,-720|_@640,0]
-1BC19 1BC42 1BC1A::[u1BC19.m@420,0|u1BC41.a@420,-200|u1BC1A.n@420,-720|_@840,0]
+1BC19 1BC42 1BC19::[u1BC19.m@420,0|u1BC41.a.0n0@420,-200|u1BC19.m@420,-720|_@640,0]
+1BC19 1BC42 1BC1A::[u1BC19.m@420,0|u1BC41.a.0n0@420,-200|u1BC1A.n@420,-720|_@840,0]
 1BC19 1BC42 1BC1B::[u1BC19.m@420,0|u1BC42.sloan_ow.0p270r@420,0|u1BC1B.j@320,0|_@1160,0]
 1BC19 1BC42 1BC1C::[u1BC19.m@420,0|u1BC42.sloan_ow.0n90r@420,-200|u1BC1C.s@320,-300|_@1160,0]
 1BC1A 1BC42 1BC02::[u1BC1A.n@220,0|u1BC42.sloan_ow.180p90r@220,-200|u1BC02.p@320,-500|_@640,0]
 1BC1A 1BC42 1BC05::[u1BC1A.n@271,0|u1BC42.sloan_ow.180p60r@271,-200|u1BC05.k@408,-563|_@691,0]
-1BC1A 1BC42 EC19::[u1BC1A.n@420,0|u1BC41.a@420,-200|uniEC19.reversed_m@420,0|_@840,0]
-1BC1A 1BC42 EC1A::[u1BC1A.n@220,0|u1BC41.a@220,-200|uniEC1A.reversed_n@220,0|_@640,0]
+1BC1A 1BC42 EC19::[u1BC1A.n@420,0|u1BC41.a.0n0@420,-200|uniEC19.reversed_m@420,0|_@840,0]
+1BC1A 1BC42 EC1A::[u1BC1A.n@220,0|u1BC41.a.0n0@220,-200|uniEC1A.reversed_n@220,0|_@640,0]
 1BC1A 1BC42 EC1B::[u1BC1A.n@740,0|u1BC42.sloan_ow.180n270r@740,0|uniEC1B.reversed_j@840,0|_@1160,0]
 1BC1A 1BC42 EC1C::[u1BC1A.n@740,0|u1BC42.sloan_ow.180p90r@740,-200|uniEC1C.reversed_s@840,-300|_@1160,0]
-1BC1A 1BC42 1BC19::[u1BC1A.n@420,0|u1BC41.a@420,-200|u1BC19.m@420,-720|_@840,0]
-1BC1A 1BC42 1BC1A::[u1BC1A.n@220,0|u1BC41.a@220,-200|u1BC1A.n@220,-720|_@640,0]
+1BC1A 1BC42 1BC19::[u1BC1A.n@420,0|u1BC41.a.0n0@420,-200|u1BC19.m@420,-720|_@840,0]
+1BC1A 1BC42 1BC1A::[u1BC1A.n@220,0|u1BC41.a.0n0@220,-200|u1BC1A.n@220,-720|_@640,0]
 1BC1A 1BC42 1BC1B::[u1BC1A.n@220,0|u1BC42.sloan_ow.180p90r@220,-200|u1BC1B.j@320,0|_@1160,0]
 1BC1A 1BC42 1BC1C::[u1BC1A.n@220,0|u1BC42.sloan_ow.180p90r@220,-200|u1BC1C.s@320,-300|_@1160,0]
 1BC1B 1BC42 1BC02::[u1BC1B.j@120,0|u1BC41.a.90n90@840,-100|u1BC02.p@840,-500|_@1160,0]

tests/default-ignorable.test
@@ -14,4 +14,4 @@
 # limitations under the License.
 
 1BC02 200C 1BC03::[u1BC02.p@120,0|uni200C.zwnj@240,0|u1BC03.t@360,0|_@980,0]
-1BC02 200D 1BC46::[u1BC02.p@120,0|u1BC47.e@120,-100|_@440,0]
+1BC02 200D 1BC46::[u1BC02.p@120,0|u1BC46.i.270p90@120,-100|_@440,0]

tests/hooks.test
@@ -13,57 +13,57 @@
 # limitations under the License.
 
 # Horizontal hook, final
-EC02 1BC7A::[uniEC02.reversed_p@120,0|u1BC47.e@120,400|_@440,0]
+EC02 1BC7A::[uniEC02.reversed_p@120,0|u1BC46.i.270p90@120,400|_@440,0]
 EC04 1BC7A::[uniEC04.reversed_f@608,0|u1BC46.i.90p270@320,500|_@728,0]
-EC19 1BC7A::[uniEC19.reversed_m@420,0|u1BC7A.e_hook@420,720|_@740,0]
+EC19 1BC7A::[uniEC19.reversed_m@420,0|u1BC46.i.90n270@420,720|_@740,0]
 EC1A 1BC7A::[uniEC1A.reversed_n@320,0|u1BC46.i.90p270@320,720|_@740,0]
-EC1B 1BC7A::[uniEC1B.reversed_j@840,0|u1BC7A.e_hook@120,0|_@960,0]
-EC1C 1BC7A::[uniEC1C.reversed_s@840,0|u1BC47.e@120,200|_@960,0]
-1BC02 1BC7A::[u1BC02.p@120,0|u1BC7A.e_hook@120,0|_@440,0]
-1BC04 1BC7A::[u1BC04.f@120,0|u1BC7A.e_hook@408,0|_@728,0]
-1BC19 1BC7A::[u1BC19.m@420,0|u1BC7A.e_hook@420,0|_@740,0]
+EC1B 1BC7A::[uniEC1B.reversed_j@840,0|u1BC46.i.90n270@120,0|_@960,0]
+EC1C 1BC7A::[uniEC1C.reversed_s@840,0|u1BC46.i.270p90@120,200|_@960,0]
+1BC02 1BC7A::[u1BC02.p@120,0|u1BC46.i.90n270@120,0|_@440,0]
+1BC04 1BC7A::[u1BC04.f@120,0|u1BC46.i.90n270@408,0|_@728,0]
+1BC19 1BC7A::[u1BC19.m@420,0|u1BC46.i.90n270@420,0|_@740,0]
 1BC1A 1BC7A::[u1BC1A.n@320,0|u1BC46.i.90p270@320,0|_@740,0]
-1BC1B 1BC7A::[u1BC1B.j@120,0|u1BC7A.e_hook@840,0|_@1160,0]
-1BC1C 1BC7A::[u1BC1C.s@120,0|u1BC47.e@840,200|_@1160,0]
+1BC1B 1BC7A::[u1BC1B.j@120,0|u1BC46.i.90n270@840,0|_@1160,0]
+1BC1C 1BC7A::[u1BC1C.s@120,0|u1BC46.i.270p90@840,200|_@1160,0]
 
 # Horizontal hook, initial
-1BC7A EC02::[u1BC7A.e_hook@120,0|uniEC02.reversed_p@320,0|_@440,0]
-1BC7A EC04::[u1BC7A.e_hook@208,0|uniEC04.reversed_f@408,0|_@528,0]
+1BC7A EC02::[u1BC46.i.90n270@120,0|uniEC02.reversed_p@320,0|_@440,0]
+1BC7A EC04::[u1BC46.i.90n270@208,0|uniEC04.reversed_f@408,0|_@528,0]
 1BC7A EC19::[u1BC46.i.90p270@620,0|uniEC19.reversed_m@420,0|_@740,0]
-1BC7A EC1A::[u1BC7A.e_hook@120,0|uniEC1A.reversed_n@320,0|_@740,0]
-1BC7A EC1B::[u1BC7A.e_hook@640,0|uniEC1B.reversed_j@840,0|_@960,0]
-1BC7A EC1C::[u1BC47.e@640,200|uniEC1C.reversed_s@840,0|_@960,0]
-1BC7A 1BC02::[u1BC47.e@120,400|u1BC02.p@320,0|_@440,0]
-1BC7A 1BC04::[u1BC7A.e_hook@120,500|u1BC04.f@320,0|_@728,0]
+1BC7A EC1A::[u1BC46.i.90n270@120,0|uniEC1A.reversed_n@320,0|_@740,0]
+1BC7A EC1B::[u1BC46.i.90n270@640,0|uniEC1B.reversed_j@840,0|_@960,0]
+1BC7A EC1C::[u1BC46.i.270p90@640,200|uniEC1C.reversed_s@840,0|_@960,0]
+1BC7A 1BC02::[u1BC46.i.270p90@120,400|u1BC02.p@320,0|_@440,0]
+1BC7A 1BC04::[u1BC46.i.90n270@120,500|u1BC04.f@320,0|_@728,0]
 1BC7A 1BC19::[u1BC46.i.90p270@620,720|u1BC19.m@420,0|_@740,0]
-1BC7A 1BC1A::[u1BC7A.e_hook@120,720|u1BC1A.n@320,0|_@740,0]
-1BC7A 1BC1B::[u1BC7A.e_hook@120,0|u1BC1B.j@320,0|_@1160,0]
-1BC7A 1BC1C::[u1BC47.e@120,200|u1BC1C.s@320,0|_@1160,0]
+1BC7A 1BC1A::[u1BC46.i.90n270@120,720|u1BC1A.n@320,0|_@740,0]
+1BC7A 1BC1B::[u1BC46.i.90n270@120,0|u1BC1B.j@320,0|_@1160,0]
+1BC7A 1BC1C::[u1BC46.i.270p90@120,200|u1BC1C.s@320,0|_@1160,0]
 
 # Vertical hook, final
 EC02 1BC7B::[uniEC02.reversed_p@220,0|u1BC46.i.180n0@220,500|_@340,0]
 EC04 1BC7B::[uniEC04.reversed_f@508,0|u1BC46.i.180n0@220,500|_@628,0]
-EC19 1BC7B::[uniEC19.reversed_m@420,0|u1BC46.i@420,520|_@540,0]
+EC19 1BC7B::[uniEC19.reversed_m@420,0|u1BC46.i.180p0@420,520|_@540,0]
 EC1A 1BC7B::[uniEC1A.reversed_n@120,0|u1BC46.i.0n180@120,520|_@540,0]
-EC1B 1BC7B::[uniEC1B.reversed_j@940,0|u1BC46.i@220,-200|_@1060,0]
+EC1B 1BC7B::[uniEC1B.reversed_j@940,0|u1BC46.i.180p0@220,-200|_@1060,0]
 EC1C 1BC7B::[uniEC1C.reversed_s@940,0|u1BC46.i.180n0@220,300|_@1060,0]
-1BC02 1BC7B::[u1BC02.p@220,0|u1BC46.i@220,-200|_@340,0]
-1BC04 1BC7B::[u1BC04.f@120,0|u1BC46.i@408,-200|_@528,0]
-1BC19 1BC7B::[u1BC19.m@420,0|u1BC46.i@420,-200|_@540,0]
+1BC02 1BC7B::[u1BC02.p@220,0|u1BC46.i.180p0@220,-200|_@340,0]
+1BC04 1BC7B::[u1BC04.f@120,0|u1BC46.i.180p0@408,-200|_@528,0]
+1BC19 1BC7B::[u1BC19.m@420,0|u1BC46.i.180p0@420,-200|_@540,0]
 1BC1A 1BC7B::[u1BC1A.n@120,0|u1BC46.i.0n180@120,-200|_@540,0]
-1BC1B 1BC7B::[u1BC1B.j@120,0|u1BC46.i@840,-200|_@960,0]
+1BC1B 1BC7B::[u1BC1B.j@120,0|u1BC46.i.180p0@840,-200|_@960,0]
 1BC1C 1BC7B::[u1BC1C.s@120,0|u1BC46.i.180n0@840,300|_@960,0]
 
 # Vertical hook, initial
 1BC7B EC02::[u1BC46.i.180n0@220,-200|uniEC02.reversed_p@220,0|_@340,0]
 1BC7B EC04::[u1BC46.i.180n0@408,-200|uniEC04.reversed_f@408,0|_@528,0]
-1BC7B EC19::[u1BC46.i@420,0|uniEC19.reversed_m@420,0|_@540,0]
+1BC7B EC19::[u1BC46.i.180p0@420,0|uniEC19.reversed_m@420,0|_@540,0]
 1BC7B EC1A::[u1BC46.i.0n180@120,0|uniEC1A.reversed_n@120,0|_@540,0]
 1BC7B EC1B::[u1BC46.i.180n0@840,-200|uniEC1B.reversed_j@840,0|_@960,0]
-1BC7B EC1C::[u1BC46.i@840,300|uniEC1C.reversed_s@840,0|_@960,0]
-1BC7B 1BC02::[u1BC46.i@220,500|u1BC02.p@220,0|_@340,0]
-1BC7B 1BC04::[u1BC46.i@220,500|u1BC04.f@220,0|_@628,0]
-1BC7B 1BC19::[u1BC46.i@420,720|u1BC19.m@420,0|_@540,0]
+1BC7B EC1C::[u1BC46.i.180p0@840,300|uniEC1C.reversed_s@840,0|_@960,0]
+1BC7B 1BC02::[u1BC46.i.180p0@220,500|u1BC02.p@220,0|_@340,0]
+1BC7B 1BC04::[u1BC46.i.180p0@220,500|u1BC04.f@220,0|_@628,0]
+1BC7B 1BC19::[u1BC46.i.180p0@420,720|u1BC19.m@420,0|_@540,0]
 1BC7B 1BC1A::[u1BC46.i.0n180@120,720|u1BC1A.n@120,0|_@540,0]
 1BC7B 1BC1B::[u1BC46.i.180n0@220,-200|u1BC1B.j@220,0|_@1060,0]
-1BC7B 1BC1C::[u1BC46.i@220,300|u1BC1C.s@220,0|_@1060,0]
+1BC7B 1BC1C::[u1BC46.i.180p0@220,300|u1BC1C.s@220,0|_@1060,0]

tests/overlaps.test
@@ -92,12 +92,12 @@
 
 # Chinook Jargon abbreviations and similar constructions
 1BC03 1BCA1 1BC19::[u1BC03.t@120,0|u1BC19.m@713,-540|_@833,0]
-1BC06 1BC46 1BC05 1BC1C 1BCA1 1BC05::[u1BC06.l@120,0|u1BC47.e.45n225@620,359|u1BC05.k@761,-141|u1BC1C.s.60@473,-441|u1BC05.k@749,-734|_@1313,0]
+1BC06 1BC46 1BC05 1BC1C 1BCA1 1BC05::[u1BC06.l@120,0|u1BC46.i.45n225@620,359|u1BC05.k@761,-141|u1BC1C.s.60@473,-441|u1BC05.k@749,-734|_@1313,0]
 1BC1B 1BCA1 1BC05::[u1BC1B.j.60@120,0|u1BC05.k@756,-73|_@960,0]
 1BC1B 1BCA1 1BC07::[u1BC1B.j.90@120,0|u1BC07.b@480,-450|_@960,0]
 1BC1C 1BCA1 1BC03::[u1BC1C.s.0@120,0|u1BC03.t@625,150|_@1245,0]
 1BC1C 1BCA1 1BC07::[u1BC1C.s.90@120,0|u1BC07.b@480,-750|_@960,0]
 1BC1C 1BCA1 1BC07 1BCA1 1BC1B::[u1BC1C.s.90@120,0|u1BC07.b@480,-750|u1BC1B.j.90@120,-800|_@960,0]
 1BC1C 1BCA1 1BC1C::[u1BC1C.s@120,0|u1BC1C.s@480,0|_@1320,0]
-1BC47 1BC03 1BCA1 1BC1C::[u1BC46.i@220,0|u1BC03.t@220,0|u1BC1C.s.0@505,-150|_@1345,0]
-1BC47 1BC03 1BCA1 1BC1C 1BCA1 1BC04::[u1BC46.i@220,0|u1BC03.t@220,0|u1BC1C.s@373,-40|u1BC04.f@817,-333|_@1225,0]
+1BC47 1BC03 1BCA1 1BC1C::[u1BC46.i.180p0@220,0|u1BC03.t@220,0|u1BC1C.s.0@505,-150|_@1345,0]
+1BC47 1BC03 1BCA1 1BC1C 1BCA1 1BC04::[u1BC46.i.180p0@220,0|u1BC03.t@220,0|u1BC1C.s@373,-40|u1BC04.f@817,-333|_@1225,0]

tests/pseudo-cursive.test
@@ -59,5 +59,6 @@
 1BC02 1BC00 202F 1BC00 EC02::[u1BC02.p@120,0|u1BC00.h@276,-1|uni202F.nnbsp@396,35|u1BC00.h@546,-1|uniEC02.reversed_p@702,0|_@822,0]
 1BC02 1BC00 1BC00 202F EC02::[u1BC02.p@120,0|u1BC00.h@276,-1|u1BC00.h@432,-1|uni202F.nnbsp@552,35|uniEC02.reversed_p@702,0|_@822,0]
 
-# Regression test
+# Regression tests
 1BC01 1BC41 1BC06::[u1BC01.x@160,-169|u1BC41.a.45p45@504,-29|u1BC06.l@504,0|_@1124,0]
+1BC01 1BC47 1BC02::[u1BC01.x@160,331|u1BC46.i.90p270@536,500|u1BC02.p@336,0|_@656,0]

commit 4bbf282bbe1f37f4edb25140f3acf28cc292f49f

Tweak schema groups to fix initial diphthongs
They were broken by df2d59d7e8de4c6f142c39f6c454068c64fb82f3.

font/duployan.py
@@ -3126,6 +3126,8 @@ class Schema:
             self.glyph_class,
             self.context_in == NO_CONTEXT and not self.diphthong_1,
             self.context_out == NO_CONTEXT and not self.diphthong_2,
+            self.context_in == NO_CONTEXT and self.diphthong_1 and isinstance(self.path, Circle),
+            self.context_out == NO_CONTEXT and self.diphthong_2 and isinstance(self.path, Circle),
             self.diphthong_1,
             self.diphthong_2,
         )

tests/orienting-sequences.test
@@ -110,14 +110,14 @@
 
 # Circle and semicircle, isolated
 1BC41 1BC46::[u1BC41.a.180p180.diph1@320,0|u1BC46.i.0p180.diph2@320,200|_@440,0]
-1BC42 1BC46::[u1BC41.a.0n0.diph1@220,0|u1BC46.i.180n0.diph2@120,200|_@440,0]
+1BC42 1BC46::[u1BC42.sloan_ow.0n0r.diph1@120,0|u1BC46.i.180n0.diph2@120,200|_@440,0]
 1BC41 1BC47::[u1BC41.a@220,0|u1BC46.i.180n0@220,200|_@440,0]
 1BC42 1BC47::[u1BC41.a@220,0|u1BC46.i.0p180@220,200|_@440,0]
 1BC41 1BC4A::[u1BC41.a.90n270.circle@120,0|u1BC4A.ui@320,100|_@640,0]
 1BC42 1BC4A::[u1BC41.a.90n270.circle@120,0|u1BC4A.ui@320,100|_@640,0]
 
 # Circle and semicircle, initial
-1BC41 1BC46 1BC02::[u1BC41.a.270n270.diph1@320,400|u1BC46.i.90n270.diph2@320,500|u1BC02.p@520,0|_@640,0]
+1BC41 1BC46 1BC02::[u1BC41.a.270n270.diph1@220,400|u1BC46.i.90n270.diph2@320,500|u1BC02.p@520,0|_@640,0]
 1BC42 1BC46 1BC02::[u1BC41.a.90n270.circle@120,400|u1BC46.i.90n270@320,500|u1BC02.p@520,0|_@640,0]
 1BC41 1BC47 1BC02::[u1BC41.a.270p270.diph1@420,400|u1BC46.i.90p270.diph2@320,500|u1BC02.p@120,0|_@640,0]
 1BC42 1BC47 1BC02::[u1BC41.a.90p270.circle@520,400|u1BC46.i.90p270@320,500|u1BC02.p@120,0|_@640,0]
@@ -131,7 +131,7 @@
 1BC02 1BC42 1BC47::[u1BC02.p@320,0|u1BC41.a.90p90@320,-100|u1BC46.i.270p90@320,-100|_@640,0]
 
 # Circle and semicircle, medial
-1BC02 1BC41 1BC46 1BC02::[u1BC02.p@420,0|u1BC41.a.270p270.diph1@420,-200|u1BC46.i.90p270.diph2@320,-100|u1BC02.p@120,-600|_@640,0]
+1BC02 1BC41 1BC46 1BC02::[u1BC02.p@320,0|u1BC41.a.270p270.diph1@320,-100|u1BC46.i.90p270.diph2@320,0|u1BC02.p@120,-500|_@640,0]
 1BC02 1BC42 1BC46 1BC02::[u1BC02.p@320,0|u1BC41.a.270n270.diph1@320,-100|u1BC46.i.90n270.diph2@320,0|u1BC02.p@520,-500|_@640,0]
 1BC03 1BC41 1BC46 1BC02::[u1BC03.t@120,700|u1BC41.a.0p0.diph1@620,700|u1BC46.i.180p0.diph2@520,500|u1BC02.p@620,0|_@840,0]
 1BC03 1BC42 1BC46 1BC02::[u1BC03.t@120,300|u1BC41.a.0n0.diph1@620,100|u1BC46.i.180n0.diph2@520,300|u1BC02.p@620,0|_@840,0]
@@ -167,13 +167,13 @@
 1BC02 1BC4A 1BC42::[u1BC02.p@120,0|u1BC4A.ui@120,0|u1BC41.a.90n270.circle@320,-100|_@640,0]
 
 # Semicircle and circle, medial
-1BC02 1BC46 1BC41 1BC02::[u1BC02.p@120,0|u1BC46.i.270p90.diph1@120,-100|u1BC41.a.270p270.diph2@320,-100|u1BC02.p@420,-600|_@640,0]
+1BC02 1BC46 1BC41 1BC02::[u1BC02.p@120,0|u1BC46.i.270p90.diph1@120,-100|u1BC41.a.270p270.diph2@320,-100|u1BC02.p@320,-500|_@640,0]
 1BC02 1BC46 1BC42 1BC02::[u1BC02.p@120,0|u1BC46.i.270p90@120,-100|u1BC41.a.90n90@320,-100|u1BC02.p@320,-500|_@640,0]
 1BC02 1BC46 1BC41 1BC03::[u1BC02.p@120,0|u1BC46.i.270p90.diph1@120,-100|u1BC41.a.270p0.diph2.circle@320,-100|u1BC03.t@420,-100|_@1040,0]
 1BC02 1BC46 1BC42 1BC03::[u1BC02.p@120,0|u1BC46.i.270p90@120,-100|u1BC42.sloan_ow.90n180r@320,-100|u1BC03.t@320,-100|_@940,0]
 1BC02 1BC46 1BC41 1BC06::[u1BC02.p@120,0|u1BC46.i.270p90.diph1@120,-100|u1BC41.a.270p45.diph2.circle@320,-100|u1BC06.l@491,-71|_@1111,0]
 1BC02 1BC46 1BC42 1BC06::[u1BC02.p@120,0|u1BC46.i.270p90@120,-100|u1BC42.sloan_ow.90n225r@320,-241|u1BC06.l@420,-141|_@1040,0]
-1BC02 1BC47 1BC41 1BC02::[u1BC02.p@520,0|u1BC46.i.270n90.diph1@520,-100|u1BC41.a.270n270.diph2@320,-100|u1BC02.p@220,-600|_@640,0]
+1BC02 1BC47 1BC41 1BC02::[u1BC02.p@520,0|u1BC46.i.270n90.diph1@520,-100|u1BC41.a.270n270.diph2@320,-100|u1BC02.p@320,-500|_@640,0]
 1BC02 1BC47 1BC42 1BC02::[u1BC02.p@520,0|u1BC46.i.270n90@520,-100|u1BC41.a.90p90@320,-100|u1BC02.p@320,-500|_@640,0]
 1BC02 1BC47 1BC41 1BC03::[u1BC02.p@120,0|u1BC46.i.270p90.diph1@120,-100|u1BC41.a.270p0.diph2.circle@320,-100|u1BC03.t@420,-100|_@1040,0]
 1BC02 1BC47 1BC42 1BC03::[u1BC02.p@120,0|u1BC46.i.270p90@120,-100|u1BC42.sloan_ow.90n180r@320,-100|u1BC03.t@320,-100|_@940,0]