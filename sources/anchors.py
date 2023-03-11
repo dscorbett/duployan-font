dscorbett/duployan-font

commit c7edd204b5ded27315dd4859f8f4e23118ec9f93

Let all above- and below-base marks support 'mkmk'

sources/anchors.py
@@ -73,15 +73,15 @@ SECANT: Final[str] = 'sec'
 
 
 ALL_MKMK: Final[list[str]] = [
-    RELATIVE_1,
-    RELATIVE_2,
-    MIDDLE,
     ABOVE,
     BELOW,
 ]
 
 
 ALL_MARK: Final[list[str]] = ALL_MKMK + [
+    RELATIVE_1,
+    RELATIVE_2,
+    MIDDLE,
     SECANT,
 ]
 

sources/duployan.py
@@ -734,6 +734,26 @@ class Builder:
                 glyph.left_side_bearing = side_bearing
         self._wrangle_anchor_points(schema, glyph, cmapped_anchors, stroke_width)
 
+    def _add_mkmk_anchor_points(
+        self,
+        schema: Schema,
+        glyph: fontforge.glyph,
+        stroke_width: float,
+    ) -> None:
+        for anchor_class_name, type, x, y in glyph.anchorPoints:
+            if type == 'mark' and schema.anchor == anchor_class_name in anchors.ALL_MKMK:
+                mkmk_anchor_class_name = mkmk(anchor_class_name)
+                glyph.addAnchorPoint(mkmk_anchor_class_name, 'mark', x, y)
+                _, y_min, _, y_max = glyph.boundingBox()
+                gap = stroke_width / 2 + self.stroke_gap + self.light_line / 2
+                match anchor_class_name:
+                    case anchors.ABOVE:
+                        y = y_max + gap
+                    case anchors.BELOW:
+                        y = y_min - gap
+                glyph.addAnchorPoint(mkmk_anchor_class_name, 'basemark', x, y)
+                return
+
     def _wrangle_anchor_points(
         self,
         schema: Schema,
@@ -741,6 +761,8 @@ class Builder:
         cmapped_anchors: Set[str],
         stroke_width: float,
     ) -> None:
+        if schema.anchor:
+            self._add_mkmk_anchor_points(schema, glyph, stroke_width)
         if schema.glyph_class == GlyphClass.MARK or isinstance(schema.path, Notdef):
             return
         anchor_tests = {anchor: anchor in cmapped_anchors or anchor in schema.anchors for anchor in anchors.ALL_MARK}

sources/shapes.py
@@ -1396,7 +1396,6 @@ class Dot(Shape):
         pen.lineTo((0, 0))
         glyph.stroke('circular', stroke_width, 'round')
         if anchor:
-            glyph.addAnchorPoint(mkmk(anchor), 'mark', *_rect(0, 0))
             glyph.addAnchorPoint(anchor, 'mark', *_rect(0, 0))
         elif joining_type != Type.NON_JOINING:
             glyph.addAnchorPoint(anchors.CURSIVE, 'entry', 0, 0 if self.centered else -(stroke_width / 2))
@@ -1595,7 +1594,6 @@ class Line(Shape):
             elif (anchor == anchors.ABOVE) == (self.angle < 180):
                 length = 0
             glyph.addAnchorPoint(anchor, 'mark', length, end_y)
-            glyph.addAnchorPoint(mkmk(anchor), 'mark', length, end_y)
         elif self.secant:
             glyph.addAnchorPoint(anchors.CONTINUING_OVERLAP, 'exit', length * self.secant, end_y)
             glyph.addAnchorPoint(anchors.PRE_HUB_CONTINUING_OVERLAP, 'exit', length * self.secant, end_y)
@@ -2069,7 +2067,6 @@ class Curve(Shape):
         anchor_name = mkmk if child else lambda a: a
         if anchor:
             glyph.addAnchorPoint(anchor, 'mark', *_rect(r, math.radians(relative_mark_angle)))
-            glyph.addAnchorPoint(mkmk(anchor), 'mark', *_rect(r, math.radians(relative_mark_angle)))
         else:
             base = 'basemark' if child else 'base'
             if joining_type != Type.NON_JOINING:
@@ -3172,11 +3169,9 @@ class Complex(Shape):
         elif anchor == anchors.ABOVE:
             glyph.addAnchorPoint(anchor, 'mark', x_center, y_min + stroke_width / 2)
             glyph.addAnchorPoint(mkmk(anchor), 'basemark', x_center, y_max + stroke_width / 2 + stroke_gap + light_line / 2)
-            glyph.addAnchorPoint(mkmk(anchor), 'mark', x_center, y_min + stroke_width / 2)
         elif anchor == anchors.BELOW:
             glyph.addAnchorPoint(anchor, 'mark', x_center, y_max - stroke_width / 2)
             glyph.addAnchorPoint(mkmk(anchor), 'basemark', x_center, y_min - (stroke_width / 2 + stroke_gap + light_line / 2))
-            glyph.addAnchorPoint(mkmk(anchor), 'mark', x_center, y_max - stroke_width / 2)
         return first_is_invisible
 
     def can_be_child(self, size: float) -> bool:

tests/diacritics.test
@@ -30,10 +30,20 @@
 0021 0331::[exclam@0,0|uni0331.macron_below.wl@84,-175|_@268,0]
 
 # U+25CC DOTTED CIRCLE
-25CC 0300::[uni25CC.dotted_circle@0,0|uni0300.grave.wl@343,654|_@686,0]
-25CC 0316::[uni25CC.dotted_circle@0,0|uni0316.grave_below.wl@430,-192|_@686,0]
-25CC 20DD::[uni25CC.dotted_circle@0,0|uni20DD.enclosing_circle.wl@343,-244|_@686,0]
-25CC 1BC9E::[uni25CC.dotted_circle@0,0|dupl.Line.0.dotted@686,0|u1BC9E.double.90.wl@1181,-112|_@1676,0]
+25CC 0300 0300::[uni25CC.dotted_circle@0,0|uni0300.grave.wl@343,654|uni0300.grave.wl@343,879|_@686,0]
+25CC 0301 0301::[uni25CC.dotted_circle@0,0|uni0301.acute.wl@343,654|uni0301.acute.wl@343,900|_@686,0]
+25CC 0302 0302::[uni25CC.dotted_circle@0,0|uni0302.circumflex.wl@253,654|uni0302.circumflex.wl@253,871|_@686,0]
+25CC 0304 0304::[uni25CC.dotted_circle@0,0|uni0304.macron.wl@293,654|uni0304.macron.wl@293,829|_@686,0]
+25CC 0306 0306::[uni25CC.dotted_circle@0,0|uni0306.breve.wl@343,654|uni0306.breve.wl@343,879|_@686,0]
+25CC 0307 0307::[uni25CC.dotted_circle@0,0|uni0307.dot_above.wl@343,640|uni0307.dot_above.wl@343,830|_@686,0]
+25CC 0308 0308::[uni25CC.dotted_circle@0,0|uni0308.diaeresis.wl@272,654|uni0308.diaeresis.wl@272,857|_@686,0]
+25CC 030C 030C::[uni25CC.dotted_circle@0,0|uni030C.caron.wl@253,654|uni030C.caron.wl@253,871|_@686,0]
+25CC 0316 0316::[uni25CC.dotted_circle@0,0|uni0316.grave_below.wl@430,-192|uni0316.grave_below.wl@430,-417|_@686,0]
+25CC 0317 0317::[uni25CC.dotted_circle@0,0|uni0317.acute_below.wl@272,-213|uni0317.acute_below.wl@272,-459|_@686,0]
+25CC 0323 0323::[uni25CC.dotted_circle@0,0|uni0323.dot_below.wl@343,-156|uni0323.dot_below.wl@343,-345|_@686,0]
+25CC 0324 0324::[uni25CC.dotted_circle@0,0|uni0324.diaeresis_below.wl@272,-170|uni0324.diaeresis_below.wl@272,-373|_@686,0]
+25CC 032F 032F::[uni25CC.dotted_circle@0,0|uni032F.inverted_breve_below.wl@343,-192|uni032F.inverted_breve_below.wl@343,-417|_@686,0]
+25CC 0331 0331::[uni25CC.dotted_circle@0,0|uni0331.macron_below.wl@293,-142|uni0331.macron_below.wl@293,-317|_@686,0]
 
 # <U+1BC03 DUPLOYAN LETTER T, U+1BC9E DUPLOYAN DOUBLE MARK>
 1BC03 1BC9E 0300::[u1BC03.t@0,0|u1BC9E.double.90@370,-112|uni0300.grave@370,245|_@740,0]

commit 011cfcb625dab8e816fe537e596b485a0fa35466

Fix i‘jam on child glyphs
This was broken by c7edd204b5ded27315dd4859f8f4e23118ec9f93.

sources/anchors.py
@@ -73,15 +73,15 @@ SECANT: Final[str] = 'sec'
 
 
 ALL_MKMK: Final[list[str]] = [
+    RELATIVE_1,
+    RELATIVE_2,
+    MIDDLE,
     ABOVE,
     BELOW,
 ]
 
 
 ALL_MARK: Final[list[str]] = ALL_MKMK + [
-    RELATIVE_1,
-    RELATIVE_2,
-    MIDDLE,
     SECANT,
 ]
 

sources/duployan.py
@@ -751,6 +751,8 @@ class Builder:
                         y = y_max + gap
                     case anchors.BELOW:
                         y = y_min - gap
+                    case _:
+                        continue
                 glyph.addAnchorPoint(mkmk_anchor_class_name, 'basemark', x, y)
                 return
 

tests/overlaps.test
@@ -81,6 +81,10 @@
 # Marks do not block trees.
 1BC25 0302 1BCA1 1BC04::[u1BC1C.s@0,0|dupl.Dot.rel1@346,286|uni0302.circumflex@269,405|u1BC04.f@444,-293|_@972,0]
 
+# Trees do not interfere with marks.
+1BC37 1BCA0 1BC1F 1BC33::[u1BC37.t_r_s@0,0|u1BC1B.j.sub@340,40|u1BC9E.double.90@700,228|u1BC33.s_t_r@600,-600|_@1440,0]
+1BC37 1BCA0 1BC24 1BC33::[u1BC37.t_r_s@14,0|u1BC1B.j.sub@354,40|dupl.Dot.rel1@700,26|dupl.Dot.rel2@700,480|u1BC33.s_t_r@614,-600|_@1454,0]
+
 # U+003D EQUALS SIGN is a valid root, but does not have other tree or
 # cursive behavior.
 003D 1BCA0 1BC02::[uni200C.zwnj@0,0|equal@0,0|u1BC02.p@333,27|_@740,0]