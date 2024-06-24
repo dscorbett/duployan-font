dscorbett/duployan-font

commit b42983a3059dac6522336d0004dc09db6ded9ce8

Optimize by using reverse chaining substitutions

sources/phases/main.py
@@ -1377,6 +1377,7 @@ def join_with_next(
         'rclt',
         'dflt',
         mark_filtering_set=phases.CONTINUING_OVERLAP_CLASS,
+        reversed=True,
     )
     lookup = Lookup(
         'rclt',
@@ -1449,6 +1450,7 @@ def join_circle_with_adjacent_nonorienting_glyph(
         'rclt',
         'dflt',
         mark_filtering_set='ignored_for_topography',
+        reversed=True,
     )
     if len(original_schemas) != len(schemas):
         return [lookup]
@@ -1786,6 +1788,7 @@ def create_diagonal_fractions(
     lookup_slash = Lookup(
         'rlig',
         'dflt',
+        reversed=True,
     )
     lookup_dnom = Lookup(
         'rlig',

sources/phases/marker.py
@@ -247,6 +247,7 @@ def shrink_wrap_enclosing_circle(
         'rlig',
         'dflt',
         mark_filtering_set='all',
+        reversed=True,
     )
     dist_lookup = Lookup(
         'abvm',
@@ -586,6 +587,7 @@ def remove_false_end_markers(
         'dist',
         'dflt',
         flags=fontTools.otlLib.builder.LOOKUP_FLAG_IGNORE_LIGATURES + fontTools.otlLib.builder.LOOKUP_FLAG_IGNORE_MARKS,
+        reversed=True,
     )
     if len(original_schemas) != len(schemas):
         return [lookup]

commit 8fa9745ee6949cd289cf5102effa3df6b1db6d78

Fix sequences of three or more circles
They were broken by b42983a3059dac6522336d0004dc09db6ded9ce8. That
commit’s optimization of using a reverse chaining substitution is still
a good idea, so the solution is not to revert it; rather, it is to
remove a different optimization. Like many phases, the buggy one,
`join_circle_with_adjacent_nonorienting_glyph`, used the following
optimization to effectively run only once:

```python
    if len(original_schemas) != len(schemas):
        return [lookup]
```

That is kind of a hack because the phase system runs each phase until
the set of schemas reaches a fixed point. Returning early on non-initial
iterations does obviously produce a fixed point, but it goes against the
spirit of the thing. It should only be used to save computation time if
running an extra iteration would have no effect. However, nothing
actually verifies that it would have no effect, and in this case, that
led to a bug.

The real solution is therefore to make the phase correctly process
schemas that were created in previous iterations. Returning early
happened to work when the phase produced a normal chaining substitution,
but it was always a risky optimization.

sources/phases/main.py
@@ -1472,12 +1472,10 @@ def join_circle_with_adjacent_nonorienting_glyph(
         mark_filtering_set='ignored_for_topography',
         reversed=True,
     )
-    if len(original_schemas) != len(schemas):
-        return [lookup]
     contexts_out: OrderedSet[Context] = OrderedSet()
     for schema in new_schemas:
         if schema.ignored_for_topography:
-            if isinstance(schema.path, Circle):
+            if schema.context_out == NO_CONTEXT and isinstance(schema.path, Circle):
                 classes['i'].append(schema)
             classes['ignored_for_topography'].append(schema)
         elif (schema.glyph_class == GlyphClass.JOINER

tests/orienting-sequences.test
@@ -1,5 +1,5 @@
 # Copyright 2020-2021 Google LLC
-# Copyright 2022-2023 David Corbett
+# Copyright 2022-2024 David Corbett
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -78,6 +78,54 @@
 1BC02 1BC52 1BC53 1BC02::[u1BC02.p@600,0|u1BC51.u.270n180@300,-300|u1BC51.u.180n90@0,-300|dupl_.Dot.1.rel1@237,-63|u1BC02.p@0,-500|_@840,0]
 1BC03 1BC53 1BC52 1BC02::[u1BC03.t@0,0|u1BC51.u.0p90@500,0|dupl_.Dot.1.rel1@535,237|u1BC51.u.90n0@800,300|u1BC02.p@1100,100|_@1340,0]
 1BC03 1BC52 1BC53 1BC02::[u1BC03.t@0,300|u1BC51.u.0n270@500,0|u1BC51.u.270n180@500,-300|dupl_.Dot.1.rel1@535,-63|u1BC02.p@500,-800|_@1040,0]
+
+# Two circles, isolated
+1BC44 1BC42::[u1BC44.o@0,0|u1BC41.a.90n270.circle@300,50|_@740,0]
+1BC42 1BC42::[u1BC41.a@0,0|u1BC41.a.90n270.circle@200,0|_@640,0]
+1BC42 1BC44::[u1BC41.a@0,0|u1BC44.o.90n270.circle@200,-50|_@740,0]
+1BC44 1BC44::[u1BC44.o@0,0|u1BC44.o.90n270.circle@300,0|_@840,0]
+
+# Two circles, initial
+1BC44 1BC42 1BC02::[u1BC44.o.270n90.circle@200,350|u1BC41.a.90p90@0,400|u1BC02.p@200,0|_@740,0]
+1BC42 1BC42 1BC02::[u1BC41.a.270n90.circle@200,400|u1BC41.a.90p90@0,400|u1BC02.p@200,0|_@640,0]
+1BC42 1BC44 1BC02::[u1BC41.a.90n270.circle@0,400|u1BC44.o.90n90@200,350|u1BC02.p@200,0|_@740,0]
+1BC44 1BC44 1BC02::[u1BC44.o.90n270.circle@0,350|u1BC44.o.90n90@300,350|u1BC02.p@300,0|_@840,0]
+
+# Two circles, final
+1BC02 1BC44 1BC42::[u1BC02.p@200,0|u1BC44.o.90n90@200,-150|u1BC41.a.270n90.circle@0,-100|_@740,0]
+1BC02 1BC42 1BC42::[u1BC02.p@200,0|u1BC41.a.90p90@0,-100|u1BC41.a.90n270.circle@200,-100|_@640,0]
+1BC02 1BC42 1BC44::[u1BC02.p@200,0|u1BC41.a.90p90@0,-100|u1BC44.o.90n270.circle@200,-150|_@740,0]
+1BC02 1BC44 1BC44::[u1BC02.p@300,0|u1BC44.o.90n90@300,-150|u1BC44.o.270n90.circle@0,-150|_@840,0]
+
+# Two circles, medial
+1BC02 1BC44 1BC42 1BC02::[u1BC02.p@200,0|u1BC44.o.90n90@200,-150|u1BC41.a.90p90@0,-100|u1BC02.p@200,-500|_@740,0]
+1BC02 1BC42 1BC42 1BC02::[u1BC02.p@200,0|u1BC41.a.90p90@0,-100|u1BC41.a.90n90@200,-100|u1BC02.p@200,-500|_@640,0]
+1BC02 1BC42 1BC44 1BC02::[u1BC02.p@200,0|u1BC41.a.90p90@0,-100|u1BC44.o.90n90@200,-150|u1BC02.p@200,-500|_@740,0]
+1BC02 1BC44 1BC44 1BC02::[u1BC02.p@300,0|u1BC44.o.90n90@300,-150|u1BC44.o.90p90@0,-150|u1BC02.p@300,-500|_@840,0]
+
+# Three circles, isolated
+1BC44 1BC42 1BC42::[u1BC44.o@0,0|u1BC41.a.90n270.circle@300,50|u1BC41.a.90n270.circle@500,50|_@940,0]
+1BC42 1BC42 1BC42::[u1BC41.a@0,0|u1BC41.a.90n270.circle@200,0|u1BC41.a.90n270.circle@400,0|_@840,0]
+1BC42 1BC44 1BC44::[u1BC41.a@0,0|u1BC44.o.90n270.circle@200,-50|u1BC44.o.90n270.circle@500,-50|_@1040,0]
+1BC44 1BC44 1BC44::[u1BC44.o@0,0|u1BC44.o.90n270.circle@300,0|u1BC44.o.90n270.circle@600,0|_@1140,0]
+
+# Three circles, initial
+1BC42 1BC42 1BC44 1BC02::[u1BC41.a.90n270.circle@0,400|u1BC41.a.90n270.circle@200,400|u1BC44.o.90n90@400,350|u1BC02.p@400,0|_@940,0]
+1BC42 1BC42 1BC42 1BC02::[u1BC41.a.270n90.circle@400,400|u1BC41.a.270n90.circle@200,400|u1BC41.a.90p90@0,400|u1BC02.p@200,0|_@840,0]
+1BC44 1BC44 1BC42 1BC02::[u1BC44.o.270n90.circle@500,350|u1BC44.o.270n90.circle@200,350|u1BC41.a.90p90@0,400|u1BC02.p@200,0|_@1040,0]
+1BC44 1BC44 1BC44 1BC02::[u1BC44.o.90n270.circle@0,350|u1BC44.o.90n270.circle@300,350|u1BC44.o.90n90@600,350|u1BC02.p@600,0|_@1140,0]
+
+# Three circles, final
+1BC02 1BC44 1BC42 1BC42::[u1BC02.p@400,0|u1BC44.o.90n90@400,-150|u1BC41.a.270n90.circle@200,-100|u1BC41.a.270n90.circle@0,-100|_@940,0]
+1BC02 1BC42 1BC42 1BC42::[u1BC02.p@200,0|u1BC41.a.90p90@0,-100|u1BC41.a.90n270.circle@200,-100|u1BC41.a.90n270.circle@400,-100|_@840,0]
+1BC02 1BC42 1BC44 1BC44::[u1BC02.p@200,0|u1BC41.a.90p90@0,-100|u1BC44.o.90n270.circle@200,-150|u1BC44.o.90n270.circle@500,-150|_@1040,0]
+1BC02 1BC44 1BC44 1BC44::[u1BC02.p@600,0|u1BC44.o.90n90@600,-150|u1BC44.o.270n90.circle@300,-150|u1BC44.o.270n90.circle@0,-150|_@1140,0]
+
+# Three circles, medial
+1BC02 1BC44 1BC42 1BC42 1BC02::[u1BC02.p@400,0|u1BC44.o.90n90@400,-150|u1BC41.a.270n90.circle@200,-100|u1BC41.a.90p90@0,-100|u1BC02.p@200,-500|_@940,0]
+1BC02 1BC42 1BC42 1BC42 1BC02::[u1BC02.p@200,0|u1BC41.a.90p90@0,-100|u1BC41.a.90n270.circle@200,-100|u1BC41.a.90n90@400,-100|u1BC02.p@400,-500|_@840,0]
+1BC02 1BC42 1BC44 1BC44 1BC02::[u1BC02.p@200,0|u1BC41.a.90p90@0,-100|u1BC44.o.90n270.circle@200,-150|u1BC44.o.90n270.circle@500,-150|u1BC02.p@800,-500|_@1040,0]
+1BC02 1BC44 1BC44 1BC44 1BC02::[u1BC02.p@600,0|u1BC44.o.90n90@600,-150|u1BC44.o.270n90.circle@300,-150|u1BC44.o.270n90.circle@0,-150|u1BC02.p@0,-500|_@1140,0]
 
 # Semicircle and quarter-circle, isolated
 1BC46 1BC53::[u1BC46.i@0,0|u1BC51.u.0p90@100,0|dupl_.Dot.1.rel1@135,237|_@640,0]